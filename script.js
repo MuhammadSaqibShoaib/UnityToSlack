c30ba9e0e9790d2c1f8eab7046b425c0949aea10
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2023-09-12 17:56:17 +0500

    updated html and js

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ function handleURLParameters() {
 
     // Access individual parameters by their key
     const code = urlParams.get('code');
-
     // Get the reference to the <h1> element
     const h1Element = document.getElementById('output');
 
     // Update the content of the <h1> element with the values
-    h1Element.textContent = `code: ${code || 'Not provided'}`;
+    h1Element.textContent = `code: ${'Got it' || 'Not provided'}`;
+
+    fetch("https://ruby-zealous-bandicoot.cyclic.app/", {
+        method: "POST",
+        body: JSON.stringify({
+            code: code
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => {return response.json()})
+    .then((data) => console.log(data))
+    .catch(err => console.log(err))
 }
 
 // Call the function when the script is loaded
